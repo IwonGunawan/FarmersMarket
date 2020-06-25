d501baa7b2db11e6488bdcee9c92653fc569f4d5
Iwon Gunawan <iwongunawan@gmail.com>
2020-06-25 14:51:13 +0700

    change ic_launcher and added spalsh screen

---
+++ app/src/main/java/com/farmers/market/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.farmers.market;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this, MainActivity.class);
+
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+
+            }
+        }, SPLASH_DISPLAY_TIME);
+
+    }
+
+}
